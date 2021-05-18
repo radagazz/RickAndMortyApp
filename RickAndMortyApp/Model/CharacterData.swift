ffc8fcc287855931642cc5ce78fab2f275fea883
Anurak Teerarattananukulchai <Anurak.teera@gmail.com>
2021-05-18 15:56:28 +0700

    first commit

---
+++ RickAndMortyApp/Model/CharacterData.swift
@@ -0,0 +1,14 @@
+//
+//  Character.swift
+//  RickAndMortyApp
+//
+//  Created by anurak teerarattananukulchai on 16/5/2564 BE.
+//
+
+import Foundation
+
+struct CharacterData: Codable {
+    let imageURL : String
+    let charaterName : String
+    let CharGender : String
+}
