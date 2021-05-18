3ad756bbab5985c6215331effe9ae4ea643ad493
Anurak Teerarattananukulchai <Anurak.teera@gmail.com>
2021-05-18 16:00:40 +0700

    creating project and get data from web api Next to do : convert json object to swift object

--- RickAndMortyApp/Controller/ViewController.swift
+++ RickAndMortyApp/Controller/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+ 
+    
+    
+    var characterList : [CharacterData] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getCharatorInfo()
     }
+    
+    //MARK: - Functions
+    func getCharatorInfo(){
+        
+        Alamofire.request("https://rickandmortyapi.com/api/character/").responseJSON {
+            reponse in
+            if reponse.result.isSuccess == true {
+                let responseJSON = JSON(reponse.result.value!)
+                print(responseJSON)
+                // try Jsondecoder or Swifty Json here
+                
+            }
+            else {
+                print("there is error getting Character from Server")
+            }
+        }
+        
+    }
+    
+    
+    
 
+    
+    //MARK: - TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characterList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
 }
 
